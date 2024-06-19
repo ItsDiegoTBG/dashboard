6909f46c90e7d7e3449ee543709416296145dc64
ItsDiegoTBG <>
2024-06-19 10:50:33 -0500

    Avance

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 
 import Grid from '@mui/material/Unstable_Grid2';
 import './App.css'
+import Indicator from './components/Indicator';
+import Summary from './components/Summary';
+import BasicTable from './components/BasicTable';
 
 function App() {
-  
+//https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=63162f2cb9dbc8a722518d5c48390088
 
 	return (
 	  <Grid container spacing={5}>
-	      <Grid xs={12} sm={4} md={3} lg={2}>1</Grid>
-	      <Grid xs={6} sm={4} md={3} lg={2}>2</Grid>
-	      <Grid xs={6} sm={4} md={3} lg={2}>3</Grid>
-	      <Grid xs={12} sm={4} md={3} lg={2}>4</Grid>
-	      <Grid xs={6} sm={4} md={6} lg={2}>5</Grid>
-	      <Grid xs={6} sm={4} md={6} lg={2}>6</Grid>
+		  
+	      <Grid xs={12} sm={12} md={12} lg={12}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
+		  
+	      <Grid xs={12} sm={12} md={6} lg={6}><Summary></Summary></Grid>
+	      <Grid xs={12} sm={12} md={6} lg={6}><Summary></Summary></Grid>
+	      <Grid xs={12} sm={12} md={6} lg={4}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
+	      <Grid xs={12} sm={12} md={6} lg={4}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
+	      <Grid xs={12} sm={12} md={12} lg={4}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
+		  <Grid xs={12} sm={12} md={12} lg={12}><BasicTable /></Grid>
 	    </Grid>
     )
 }
 
 export default App
+
